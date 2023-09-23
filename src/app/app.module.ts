d8e0096850675de1ba7dfce186fb1f537162e2ab
Alexandre Desroches <alex.desroches7@gmail.com>
2023-09-23 17:17:27 -0400

    init material fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,6 +17,11 @@ import { FormComponent } from './components/form/form.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import * as dayjs from 'dayjs';
+
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
   public form!: FormGroup;
 
   ngOnInit(): void {
     console.log("init form group");
-    
+    const today = dayjs();
+    const firstDayOfNextMonth = today.add(1, 'month').startOf('month').hour(today.hour());
+
     this.form = new FormGroup({
-      initialDeposit: new FormControl(''),
-      regularDeposit: new FormControl(''),
+      initialDeposit: new FormControl(200),
+      regularDeposit: new FormControl(200),
+      dateStart: new FormControl(today.toISOString()),
+      dateNextDeposit: new FormControl(firstDayOfNextMonth.toISOString()),
     });
+
+    console.log(this.form.value);
+
   }
 
   handleSubmit() {
