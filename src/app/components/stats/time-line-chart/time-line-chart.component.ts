739daa61422956f6c7674c1e3f78becf20fa0aa3
Alexandre Desroches <alex.desroches7@gmail.com>
2023-10-22 21:50:05 -0400

    improve time chart

--- src/app/components/stats/time-line-chart/time-line-chart.component.ts
+++ src/app/components/stats/time-line-chart/time-line-chart.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Chart, ChartConfiguration, ChartType} from "chart.js";
+import {Chart, ChartConfiguration, ChartType, Point, PointElement} from "chart.js";
 import Annotation from 'chartjs-plugin-annotation';
 import {IDailyData} from "../../../app.definitions";
 import {CalculatorService} from "../../../services/calculator.service";
@@ -16,16 +16,34 @@ export class TimeLineChartComponent implements OnInit {
   public lineChartType: ChartType = 'line';
   public lineChartData!: ChartConfiguration['data'];
   public lineChartOptions: ChartConfiguration['options'] = {
+    normalized: true,
+    parsing: false,
+    animation: false,
+    maintainAspectRatio: false,
+    interaction: {
+      mode: 'nearest',
+      axis: 'x',
+      intersect: false
+    },
     elements: {
-      line: {},
+      line: {
+        cubicInterpolationMode: "default"
+      },
     },
     scales: {
+      x: {
+      },
       y: {
         position: 'left',
+        beginAtZero: false
       },
     },
     plugins: {
       legend: {display: true},
+      decimation: {
+        enabled: true,
+        algorithm: 'min-max',
+      },
       annotation: {
         annotations: [
           // {
@@ -48,6 +66,7 @@ export class TimeLineChartComponent implements OnInit {
       },
     },
   };
+  public colors = ["#1565C0", "#379af9", "#1bde8d", "#e8bc5c"];
 
   constructor(private readonly calculator: CalculatorService) {
     Chart.register(Annotation);
@@ -58,6 +77,8 @@ export class TimeLineChartComponent implements OnInit {
     this.subscription = this.calculator.calculationEmitter.subscribe(() => {
       this.setChartData();
     })
+
+    console.log();
   }
 
   public get dailyData(): IDailyData[] {
@@ -69,19 +90,62 @@ export class TimeLineChartComponent implements OnInit {
       labels: this.dailyData.map(daily => daily.date),
       datasets: [
         {
-          data: this.dailyData.map(item => item.newBalance),
+          data: this.mapDataToPoints("newBalance") as any,
           label: 'Balance (TVL)',
-          backgroundColor: '#1565C0FF',
-          borderColor: '#1565C0FF',
-          pointRadius: 3,
-          pointBackgroundColor: '#1565C0FF',
-          pointBorderColor: '#1483ed',
+          backgroundColor: this.colors[0],
+          borderColor: this.colors[0],
+          pointBackgroundColor: this.colors[0],
+          pointBorderColor: this.colors[0],
           pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: '#1565C0FF',
+          pointHoverBorderColor: this.colors[0],
           fill: false,
+          pointRadius: 0,
+          spanGaps: true,
+        },
+        {
+          data: this.mapDataToPoints("totalDeposited") as any,
+          label: 'Deposits (Invested)',
+          backgroundColor: this.colors[1],
+          borderColor: this.colors[1],
+          pointBackgroundColor: this.colors[1],
+          pointBorderColor: this.colors[1],
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: this.colors[1],
+          fill: false,
+          pointRadius: 0,
+          spanGaps: true,
+        },
+        {
+          data: this.mapDataToPoints("totalWithdrawn") as any,
+          label: 'Withdrawals (Cashed out)',
+          backgroundColor: this.colors[2],
+          borderColor: this.colors[2],
+          pointBackgroundColor: this.colors[2],
+          pointBorderColor: this.colors[2],
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: this.colors[2],
+          fill: false,
+          pointRadius: 0,
+          spanGaps: true,
+        },
+        {
+          data: this.mapDataToPoints("totalCompounded") as any,
+          label: 'Compounds (Earned Yield)',
+          backgroundColor: this.colors[3],
+          borderColor: this.colors[3],
+          pointBackgroundColor: this.colors[3],
+          pointBorderColor: this.colors[3],
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: this.colors[3],
+          fill: false,
+          pointRadius: 0,
           spanGaps: true,
         }
       ],
     };
+  }
+
+  private mapDataToPoints(prop: keyof IDailyData): { x: string, y: number }[] {
+    return this.dailyData.map((item: IDailyData) => ({x: item.date, y: item[prop] as number}))
   }
 }
